fd1d9575106e48827db5b22a82b96d0e01573671
Dun Liang <randonlang@gmail.com>
2015-08-13 15:50:30 +0800

    nothing to say

--- utils/package/install_prettyGUI.sh
+++ utils/package/install_prettyGUI.sh
@@ -1,6 +1,6 @@
 sudo add-apt-repository ppa:ravefinity-project/ppa
 sudo apt-get update
-sudo apt-get install ambiance-flat-colors
-sudo apt-get install radiance-flat-colors
+sudo apt-get install ambiance-flat-colors -y
+sudo apt-get install radiance-flat-colors -y
 
-sudo apt-get install unity-tweak-tool
+sudo apt-get install unity-tweak-tool -y
